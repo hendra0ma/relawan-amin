a027f7be7a1ba30f380adc6737c12272bcc75649
hendramaulidan <hendra0maulidan@gmail.com>
2023-10-23 01:40:52 +0700

    asdasd

--- resources/views/layouts/template/header.blade.php
+++ resources/views/layouts/template/header.blade.php
@@ -5,12 +5,15 @@
   <section id="hero" class="clearfix">
     <div class="container" data-aos="fade-up">
 
-      <div class="hero-img" data-aos="zoom-out" data-aos-delay="200">
+      <!-- <div class="hero-img" data-aos="zoom-out" data-aos-delay="200">
         <img src="{{asset('')}}assets/img/relawan_amin.png" alt="" class="img-fluid">
-      </div>
+      </div> -->
 
-      <div class="hero-info" data-aos="zoom-in" data-aos-delay="100">
+      <!-- <div class="hero-info" data-aos="zoom-in" data-aos-delay="100">
         <h2>Anies Baswedan & Muhaimin Iskandar</h2>
+        <div> -->
+        <div class="text-center text-white"data-aos="zoom-in" data-aos-delay="100">
+        <h2 style="font-size: 40px;font-weight: 700;">Anies Baswedan & Muhaimin Iskandar</h2>
         <div>
         
         </div>
